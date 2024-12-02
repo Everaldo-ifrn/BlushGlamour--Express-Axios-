034e6fb29e9a7431b478cbbaf2909833b1690458
Everaldo-ifrn <99830593+Everaldo-ifrn@users.noreply.github.com>
2024-12-02 20:44:11 -0300

    a

--- sever.js
+++ sever.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
-
+const path = require('path');
 const app = express();
 const PORT = 3000;
+
+// Middleware para processar dados de formulários
+app.use(express.urlencoded({ extended: true }));
 
 // Middleware para ler o corpo das requisições como JSON
 app.use(bodyParser.json());
 
-// Servir arquivos estáticos da pasta public (HTML, CSS, JS)
-app.use(express.static('public'));
+// Servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Rotas de autenticação
 app.use('/api/auth', authRoutes);
@@ -24,7 +27,34 @@ app.get('/register', (req, res) => {
     res.sendFile(__dirname + '/public/register.html');
 });
 
+app.post('/index.html', (req, res) => {
+    const {
+        nome,
+        cpf,
+        email,
+        senha,
+        telefone,
+        data,
+        numeroResidencia,
+        rua,
+        bairro,
+        cidade,
+        estado,
+        cep,
+        Complemento,
+    } = req.body;
+
+    
+
+    console.log("Dados recebidos:", req.body);
+
+    // Lógica para salvar os dados no banco de dados ou processá-los
+
+    // Redirecionar para a página index.html
+    res.redirect('/index.html');
+});
+
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 
